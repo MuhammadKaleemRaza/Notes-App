3ac01ee1f7598fd94819443a0c8b88ce620eb6c2
kaleem <muhammadkaleemraza58@gmail.com>
2021-11-17 13:56:13 +0500

    added the search feature into the new feature branch

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
 let addTxt = document.getElementById('addTxt')
 let addBtn = document.getElementById('addBtn')
+let search = document.getElementById('searchText')
 
 addBtn.addEventListener('click', function () {
-    let notesArray = []
+    let notesArray = []  //made an empty array to stor our notes
 
-    let notesFromLocalStorage = localStorage.getItem('notes')
+    let notesFromLocalStorage = localStorage.getItem('notes')  //get the notes from localStorage
 
     if (notesFromLocalStorage == null) {
-        notesArray = []
+        notesArray = []    //if there are not notes in the localStorage the array should be blank
     } else {
-        notesArray = JSON.parse(notesFromLocalStorage)
+        notesArray = JSON.parse(notesFromLocalStorage) // if there are notes turn the string which is save in local storage into array and store in notes array
     }
 
-    notesArray.push(addTxt.value)
+    notesArray.push(addTxt.value)  //push the value of the content into the notes array
 
-    localStorage.setItem('notes', JSON.stringify(notesArray))
+    localStorage.setItem('notes', JSON.stringify(notesArray))  //save the array into localStorage
 
-    addTxt.value = ""
+    addTxt.value = "" //after saving the content box should be blank
 
-    displayNotes()
+    displayNotes()  //call the displaynotes function
 
 })
 
@@ -44,7 +45,7 @@ function displayNotes() {
                   <p class="card-text">${element}</p>
                    <button href="#" class="btn btn-success" id="${index}" onclick="deleteNotes(this.id)">Delete Note</button>                  </div>
               </div>         `
-    })
+    })  //for every element into an array display this text
 
     if  (notesArray.length !== 0 ) {
         notesElement.innerHTML = displayedNote
@@ -68,11 +69,30 @@ function deleteNotes(index)  {
         notesArray = JSON.parse(notesFromLocalStorage)
     }
 
-    notesArray.splice(index, 1)
+    notesArray.splice(index, 1)  //delete the array item from the notes index to 1 meaning only delete one item
 
-    localStorage.setItem('notes', JSON.stringify(notesArray))
+    localStorage.setItem('notes', JSON.stringify(notesArray)) //update the local storage
 
-    displayNotes()
+    displayNotes()  //call the display noes function
 }
 
-displayNotes()
+displayNotes() //call the display notes function here because if we open or close the browser or refresh the saved notes should be there
+
+search.addEventListener('input', function() {
+    let searchValue = search.value.toLowerCase() //take the value of search value and turn it into lowercase
+
+    let noteCard = document.getElementsByClassName('cardNote') //take the div where our notes are located
+
+    Array.from(noteCard).forEach(function(element){
+        let cardTxt = element.getElementsByTagName('p')[0].innerText
+        if (cardTxt.includes(searchValue)){
+            element.style.display = "block"
+        }
+        else {
+            element.style.display = "none"
+        }
+
+    }) //make array of that div and fo every elemnt of that div grab the innertext of the paragraph if the value of the search includes the content of the para div's display should remain block if not should be none.
+    // console.log('input event fired', searchValue)
+})
+
